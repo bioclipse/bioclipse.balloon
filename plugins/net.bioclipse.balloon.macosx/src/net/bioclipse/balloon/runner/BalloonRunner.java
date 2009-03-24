c4e08725aa5c35b8b0b5b5247c9b2aab70301a48
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-03-24 21:32:38 +0000

    Major cleanup the mac os x ballon runner.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@9479 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.balloon.macosx/src/net/bioclipse/balloon/runner/BalloonRunner.java
+++ plugins/net.bioclipse.balloon.macosx/src/net/bioclipse/balloon/runner/BalloonRunner.java
@@ -13,19 +13,16 @@ package net.bioclipse.balloon.runner;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 import org.apache.log4j.Logger;
 import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.IExtension;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 
-import net.sf.jnati.NativeCodeException;
 import net.sf.jnati.proc.ProcessMonitor;
 import net.sf.jnati.proc.ProcessOutput;
 
@@ -40,47 +37,80 @@ public class BalloonRunner {
 
     private static final Logger logger =Logger.getLogger( BalloonRunner.class );
 
+    //Hardcoded constants
+    private final static String FRAGMENT_ID="net.bioclipse.balloon.macosx";
+    private final static String osarch="MACOSX";
+//    private final static String id="balloon";
+//    private final static String version="0.6.15";
+
+    //Filled from constructor
+    private long timeoutMs;
+
+    //Constructed in constructor
+    private File path;
+
+    
     /**
      * Overridden by platform specific fragments
      * @param inputfile
      * @param outputfile
      * @param numConf
-     * @return
+     * @return String errormessage or Null if success
+     * @throws TimeoutException 
+     * @throws InterruptedException 
+     * @throws ExecutionException 
      */
-    public static String runBalloon(String inputfile, String outputfile, int numConf){
+    public boolean runBalloon(String inputfile, String outputfile, int numConf) throws ExecutionException, InterruptedException, TimeoutException{
 
-        try {
-            BalloonRunner b = new BalloonRunner();
-            ProcessOutput output = b.convert( inputfile, outputfile, ""+numConf);
-            if (output==null)
-                return "Balloon failed. Probably timed out";
-            if (output.getExitValue()==0)
-                return null;
+//            BalloonRunner b = new BalloonRunner();
+//            ProcessOutput output = b.convert( inputfile, outputfile, ""+numConf);
+            
+            if (inputfile == null) {
+                throw new NullPointerException("Null input file");
+            }
+            if (outputfile == null) {
+                throw new NullPointerException("Null output file");
+            }
+            if (numConf <=0) {
+                throw new IllegalArgumentException("NumConf must be >1");
+            }
 
-            else
-                return "Balloon failed: " + output.getMessages();
+            logger.info("Running 'balloon -c " + numConf + " " + inputfile + " " + outputfile);
 
-//            System.out.println("Out: " + output.getOutput());
-//            System.out.println("Msgs: " + output.getMessages());
-//            System.out.println("exitvalue: " + output.getExitValue());
-        } catch ( Exception e ) {
-            return e.getMessage();
-        }
-        
+            final ProcessMonitor runner = createProcessRunner("-c"+  numConf , inputfile, outputfile );
+            
+//            try{
+                //Run the Process
+                ProcessOutput output = runner.runProcess();
+
+                //React on result
+                if (output==null)
+                    return false;
+                if (output.getExitValue()==0)
+                    return true;
+                else
+                    return false;
+//                    return "Balloon failed: " + output.getMessages();
+//            }catch (TimeoutException e){
+//                return "Balloon timed out: " + e.getMessage();
+//            } catch ( ExecutionException e ) {
+//                return "Balloon timed out: " + e.getMessage();
+//            } catch ( InterruptedException e ) {
+//                return "Balloon was interrupted: " + e.getMessage();
+//            }
 
     }
 
 
-    private File path;
-    private String osarch;
-    private String id;
-    private String version;
-    private long timeoutMs;
 
-
+    /**
+     * Set up a runner for Ballon on the current arch
+     * @param args
+     * @return
+     */
     private ProcessMonitor createProcessRunner(final String... args) {
 
-        // Generate command/args string
+        // Generate command/args string for different archs
         final String[] command = new String[args.length + 1];
         System.arraycopy(args, 0, command, 1, args.length);
 
@@ -105,15 +135,20 @@ public class BalloonRunner {
     }
 
     /**
-     * Constructor. Ensures native code is loaded.
-     * @throws IOException
-     * @throws NativeCodeException
-     * @throws ProcessRunnerException
+     * Constructor. Constructs path to ballon native executable
+     * @param timeoutMs Number of milisaeconds until timeout is reached
+     * @throws IOException 
      */
-    public BalloonRunner() throws Exception {
+    public BalloonRunner(Long timeoutMs) throws IOException {
 
+        if (timeoutMs<=0)
+            this.timeoutMs=20000;
+        else
+            this.timeoutMs=timeoutMs;
+
+        //Construct path to balloon for this installation
         String fragmentBase="";
-        Bundle bundle = Platform.getBundle( "net.bioclipse.balloon.macosx");
+        Bundle bundle = Platform.getBundle( FRAGMENT_ID);
         if (bundle!=null){
             URL url = FileLocator.find( bundle, new Path("/exec"),null );
             fragmentBase=FileLocator.toFileURL(url).getFile();
@@ -122,73 +157,15 @@ public class BalloonRunner {
                 return;
             }
         }else{
+            //This could be set for testing purposes with no OSGI
+//            fragmentBase="hardcoded/path/to/ballon_macosx/";
             fragmentBase="/Users/ola/bin/balloon/balloon_macosx/";
         }
 
-        this.id="balloon";
+        //Stor econstructed path to balloon
         this.path = new File(fragmentBase);
-        this.osarch = "MACOSX";
-        this.version="0.6.15";
-        this.timeoutMs=20000;
 
     }
 
 
-    public ProcessOutput convert(final String inf, final String outf, final String numConf) throws ExecutionException, InterruptedException, TimeoutException{
-        if (inf == null) {
-            throw new NullPointerException("Null input file");
-        }
-        if (outf == null) {
-            throw new NullPointerException("Null output file");
-        }
-        if (numConf == null) {
-            throw new IllegalArgumentException("NumConf must be >1");
-        }
-
-        logger.info("Running 'balloon --input-file" + inf +" --output-file" + outf + " -c" + numConf + " --strict");
-
-        final ProcessMonitor runner = createProcessRunner("-c"+  numConf , inf, outf );
-        runner.setTimeout( 10, TimeUnit.SECONDS );
-        try{
-            ProcessOutput output = runner.runProcess();
-            return output;
-        }catch (TimeoutException e){
-            return null;
-        }
-    }
-
-    public static void main( String[] args ) {
-
-        System.out.println("start");
-
-        // If there are no arguments, emit an appropriate usage message.
-        //
-        if (args.length == 0) {
-            System.out.println("Need at least one path to a file containing molecule as input");
-        }
-        else {
-            // Iterate over all the arguments.
-            //
-            for (int i = 0; i < args.length; ++i) {
-                // Construct the URI for the instance file.
-                // The argument is treated as a file path only if it denotes an existing file.
-                // Otherwise, it's directly treated as a URL.
-                //
-                File file = new File(args[i]);
-
-                try {
-                    BalloonRunner b = new BalloonRunner();
-                    ProcessOutput output = b.convert( file.getAbsolutePath(), file.getAbsolutePath(), "1");
-                    System.out.println("Out: " + output.getOutput());
-                    System.out.println("Msgs: " + output.getMessages());
-                    System.out.println("exitvalue: " + output.getExitValue());
-                } catch ( Exception e ) {
-                    e.printStackTrace();
-                    return;
-                }
-            }        
-        }
-
-        System.out.println("end");
-    }
 }
