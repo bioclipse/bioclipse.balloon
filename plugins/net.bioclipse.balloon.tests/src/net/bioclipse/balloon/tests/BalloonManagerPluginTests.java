b0e01ae623e7d308c48f9ffb866b51d8b38b4fc7
ospjuth <ospjuth@fcb5ba71-d80d-0410-8237-ba3920747fcc>
2009-05-03 09:17:01 +0000

    Added tests for SDF, SMILES, and CML files when generating 3D with balloon. SMILES currently fails.
    git-svn-id: https://bioclipse.svn.sourceforge.net/svnroot/bioclipse/bioclipse2/trunk@10041 fcb5ba71-d80d-0410-8237-ba3920747fcc

--- plugins/net.bioclipse.balloon.tests/src/net/bioclipse/balloon/tests/BalloonManagerPluginTests.java
+++ plugins/net.bioclipse.balloon.tests/src/net/bioclipse/balloon/tests/BalloonManagerPluginTests.java
@@ -18,15 +18,19 @@ import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.List;
 
 import net.bioclipse.balloon.business.Activator;
 import net.bioclipse.balloon.business.IBalloonManager;
 import net.bioclipse.cdk.business.ICDKManager;
 import net.bioclipse.cdk.domain.ICDKMolecule;
+import net.bioclipse.core.ResourcePathTransformer;
 import net.bioclipse.core.business.BioclipseException;
 
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.NullProgressMonitor;
 import org.junit.Test;
 import org.openscience.cdk.exception.CDKException;
 
@@ -43,7 +47,10 @@ public class BalloonManagerPluginTests {
         URL url=FileLocator.toFileURL(uri.toURL());
         String path=url.getFile();
         
-        ICDKMolecule mol  = cdk.loadMolecule( path );
+        IFile molfile = ResourcePathTransformer.getInstance().transform( path );
+        
+//        ICDKMolecule mol  = cdk.loadMolecule( path );
+        ICDKMolecule mol  = cdk.loadMolecule( molfile, new NullProgressMonitor() );
         assertTrue( cdk.has2d( mol ));
         assertFalse( cdk.has3d( mol ));
 
@@ -56,23 +63,131 @@ public class BalloonManagerPluginTests {
         assertTrue( file.exists() );
         assertTrue( file.canRead() );
         
-        ICDKMolecule mol2 = cdk.loadMolecule(res);
+//        ICDKMolecule mol2 = cdk.loadMolecule(res);
+        IFile molfile2 = ResourcePathTransformer.getInstance().transform( res );
+        ICDKMolecule mol2  = cdk.loadMolecule( molfile2, new NullProgressMonitor() );
+
         assertTrue( cdk.has3d( mol2 ));
         assertFalse( cdk.has2d( mol2 ));
+    }
 
-        res=ballon.generate3Dcoordinates( path );
+    @Test
+    public void testGenerate3DonCML() throws URISyntaxException, MalformedURLException, IOException, BioclipseException, CDKException, CoreException{
+        
+        IBalloonManager ballon = Activator.getDefault().getBalloonManager();
+        ICDKManager cdk = net.bioclipse.cdk.business.Activator.getDefault().getCDKManager();
+        
+        URI uri = getClass().getResource("/testFiles/0037.cml").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
+        
+        IFile molfile = ResourcePathTransformer.getInstance().transform( path );
+        
+//        ICDKMolecule mol  = cdk.loadMolecule( path );
+        ICDKMolecule mol  = cdk.loadMolecule( molfile, new NullProgressMonitor() );
+        assertTrue( cdk.has2d( mol ));
+        assertFalse( cdk.has3d( mol ));
+
+        String res=ballon.generate3Dcoordinates( path );
+        assertNotNull( "Balloon returned null", res );
 
         System.out.println("wrote file: " + res);
         
-        file=new File(res);
+        File file=new File(res);
         assertTrue( file.exists() );
         assertTrue( file.canRead() );
         
-        mol2 = cdk.loadMolecule(res);
+//        ICDKMolecule mol2 = cdk.loadMolecule(res);
+        IFile molfile2 = ResourcePathTransformer.getInstance().transform( res );
+        ICDKMolecule mol2  = cdk.loadMolecule( molfile2, new NullProgressMonitor() );
+
         assertTrue( cdk.has3d( mol2 ));
         assertFalse( cdk.has2d( mol2 ));
 
     }
+    
+    @Test
+    public void testGenerate3DonSDF() throws URISyntaxException, MalformedURLException, IOException, BioclipseException, CDKException, CoreException{
+        
+        IBalloonManager ballon = Activator.getDefault().getBalloonManager();
+        ICDKManager cdk = net.bioclipse.cdk.business.Activator.getDefault().getCDKManager();
+        
+        URI uri = getClass().getResource("/testFiles/Fragments2.sdf").toURI();
+        URL url=FileLocator.toFileURL(uri.toURL());
+        String path=url.getFile();
+        
+        IFile molfile = ResourcePathTransformer.getInstance().transform( path );
+        
+//        ICDKMolecule mol  = cdk.loadMolecule( path );
+        List<ICDKMolecule> mols  = cdk.loadMolecules( molfile, new NullProgressMonitor() );
+        System.out.println("SDF contained: " + mols.size() + " mols");
+        for ( ICDKMolecule mol : mols ) {
+            assertTrue( cdk.has2d( mol ));
+            assertFalse( cdk.has3d( mol ));
+        }
 
+        String res=ballon.generate3Dcoordinates( path );
+        assertNotNull( "Balloon returned null", res );
+
+        System.out.println("Balloon returned file: " + res);
+        
+        File file=new File(res);
+        assertTrue( file.exists() );
+        assertTrue( file.canRead() );
+
+
+//        ICDKMolecule mol2 = cdk.loadMolecule(res);
+        IFile molfile2 = ResourcePathTransformer.getInstance().transform( res );
+        List<ICDKMolecule> mols2  = cdk.loadMolecules( molfile2, new NullProgressMonitor() );
+        System.out.println("Returned SDF contained: " + mols2.size() + " mols");
+        for ( ICDKMolecule mol2 : mols2 ) {
+            assertTrue( cdk.has3d( mol2 ));
+            assertFalse( cdk.has2d( mol2 ));
+        }
+
+
+    }
+
+    @Test
+    public void testGenerate3DonSmiles() throws URISyntaxException, MalformedURLException, IOException, BioclipseException, CDKException, CoreException{
+        
+        IBalloonManager ballon = Activator.getDefault().getBalloonManager();
+        ICDKManager cdk = net.bioclipse.cdk.business.Activator.getDefault().getCDKManager();
+        
+      URI uri = getClass().getResource("/testFiles/sample30.smi").toURI();
+      URL url=FileLocator.toFileURL(uri.toURL());
+      String path=url.getFile();
+      
+      IFile molfile = ResourcePathTransformer.getInstance().transform( path );
+      
+      List<ICDKMolecule> mols  = cdk.loadSMILESFile( molfile, new NullProgressMonitor());
+        System.out.println("Input SMILES file contained: " + mols.size() + " mols");
+        for ( ICDKMolecule mol : mols ) {
+            assertFalse( cdk.has2d( mol ));
+            assertFalse( cdk.has3d( mol ));
+        }
+
+        String res=ballon.generate3Dcoordinates( path );
+        assertNotNull( "Balloon returned null", res );
+
+        System.out.println("Balloon returned file: " + res);
+        
+        File file=new File(res);
+        assertTrue( file.exists() );
+        assertTrue( file.canRead() );
+
+
+//        ICDKMolecule mol2 = cdk.loadMolecule(res);
+        IFile molfile2 = ResourcePathTransformer.getInstance().transform( res );
+        List<ICDKMolecule> mols2  = cdk.loadMolecules( molfile2, new NullProgressMonitor() );
+        System.out.println("Returned SDF contained: " + mols2.size() + " mols");
+        for ( ICDKMolecule mol2 : mols2 ) {
+            assertTrue( cdk.has3d( mol2 ));
+            assertFalse( cdk.has2d( mol2 ));
+        }
+
+
+    }
+    
 
 }
