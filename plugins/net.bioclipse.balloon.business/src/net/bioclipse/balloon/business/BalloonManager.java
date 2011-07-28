09c11ab84a208d21c1c961fde399fc04f272cf8f
Arvid Berg <goglepox@users.sourceforge.net>
2011-07-28 17:58:47 +0200

    Removed unused import

--- plugins/net.bioclipse.balloon.business/src/net/bioclipse/balloon/business/BalloonManager.java
+++ plugins/net.bioclipse.balloon.business/src/net/bioclipse/balloon/business/BalloonManager.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeoutException;
 
